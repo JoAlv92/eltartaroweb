9c94063017b8c5fad6234165db60d4cfd4a6e80f
José Alvarez <joalvarez92@gmail.com>
2020-07-05 21:02:54 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//Formulario de contacto
+(function(d, t){
+   var g = d.createElement(t),
+       s = d.getElementsByTagName(t)[0];
+   g.src = "http://es.foxyform.com/js.php?id=342403&sec_hash=3eee4d3e3b5&width=350px";
+   s.parentNode.insertBefore(g, s);
+}(document, "script"));
